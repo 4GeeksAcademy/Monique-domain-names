148bc50f06155ebe0dd9569364679a83f744294f
Monique Latil <moninilatil@gmail.com>
2024-10-21 17:58:23 +0000

    domain gen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "yours", "theirs", "our", "his", "her", "its", "their"];
+  let adj = [
+    "great",
+    "big",
+    "grumpy",
+    "lovely",
+    "shiny",
+    "fast",
+    "old",
+    "smart",
+    "strong"
+  ];
+  let noun = [
+    "cat",
+    "dog",
+    "singer",
+    "tree",
+    "car",
+    "house",
+    "computer",
+    "robot",
+    "book"
+  ];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        console.log(pronoun[p] + adj[a] + noun[n] + ".com");
+      }
+    }
+  }
 };
